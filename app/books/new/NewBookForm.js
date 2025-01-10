ae129756e64ccb2bb30bfda7aa17aa14e54fa716
masood17 <graphicgorillaz@gmail.com>
2025-01-11 00:27:13 +0200

    add NewBookForm.js radio options

--- app/books/new/NewBookForm.js
+++ app/books/new/NewBookForm.js
@@ -79,10 +79,44 @@ export default function NewBookForm() {
                 >
                     <option value="">Select a genre</option>
                     <option value="التاريخ">التاريخ</option>
-                    <option value="Non-Fiction">Non-Fiction</option>
-                    <option value="Science Fiction">Science Fiction</option>
-                    <option value="Mystery">Mystery</option>
-                    <option value="Biography">Biography</option>
+                    <option value="التراجم">التراجم</option>
+                    <option value="التشيع">التشيع</option>
+                    <option value="التصوف">التصوف</option>
+                    <option value="التفسير">التفسير</option>
+                    <option value="الصحاح (الحديث)">الصحاح (الحديث)</option>
+                    <option value="التخريج (الحديث)">التخريج (الحديث)</option>
+                    <option value="المعاجم (الحديث)">المعاجم (الحديث)</option>
+                    <option value="المسانيد (الحديث)">المسانيد (الحديث)</option>
+                    <option value="المصنفات (الحديث)">المصنفات (الحديث)</option>
+                    <option value="الموضوعات (الحديث)">الموضوعات (الحديث)</option>
+                    <option value="السنن (الحديث)">السنن (الحديث)</option>
+                    <option value="الزوائد (الحديث)">الزوائد (الحديث)</option>
+                    <option value="الفهارس (الحديث)">الفهارس (الحديث)</option>
+                    <option value="الاحكام (الحديث)">الاحكام (الحديث)</option>
+                    <option value="الفضائل (الحديث)">الفضائل (الحديث)</option>
+                    <option value="العلل (الحديث)">العلل (الحديث)</option>
+                    <option value="الصحاح الستة (الحديث)">الصحاح الستة (الحديث)</option>
+                    <option value="الموطآت (الحديث)">الموطآت (الحديث)</option>
+                    <option value="الرجال">الرجال</option>
+                    <option value="الرسائل">الرسائل</option>
+                    <option value="الطبقات">الطبقات</option>
+                    <option value="الفتاوى">الفتاوى</option>
+                    <option value="الفقه الحنبلي">الفقه الحنبلي</option>
+                    <option value="الفقه الحنفي">الفقه الحنفي</option>
+                    <option value="الفقه الشافعي">الفقه الشافعي</option>
+                    <option value="الفقه المالكي">الفقه المالكي</option>
+                    <option value="الفقه المقارن">الفقه المقارن</option>
+                    <option value="اللغة">اللغة</option>
+                    <option value="فهارس المخطوطات">فهارس المخطوطات</option>
+                    <option value="المعاملات المالية">المعاملات المالية</option>
+                    <option value="علم الكلام">علم الكلام</option>
+                    <option value="علوم القرآن">علوم القرآن</option>
+                    <option value="علوم الحديث">علوم الحديث</option>
+                    <option value="القراءات">القراءات</option>
+                    <option value="القواميس">القواميس</option>
+                    <option value="الاحوال الشخصية">الاحوال الشخصية</option>
+                    <option value="الميراث">الميراث</option>
+                    <option value="النكاح">النكاح</option>
                 </select>
             </div>
             <div>
