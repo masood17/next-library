c5a60effa340ec9dffcbc69456f3a013fce32053
masood17 <graphicgorillaz@gmail.com>
2025-07-10 00:40:05 +0200

    comment out link

--- app/page.js
+++ app/page.js
@@ -24,10 +24,10 @@ export default async function Home() {
         <div>
             <div className="flex justify-between items-center mb-6">
                 <h1 className="text-3xl font-bold">Library Catalog</h1>
-                <Link href="/dashboard/books/new" 
+                {/* <Link href="/dashboard/books/new" 
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Add New Book
-                </Link>
+                </Link> */}
             </div>
 
 
