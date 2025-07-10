4de8cbdcdebbdf499008a0e8de08f22cb68e9113
masood17 <graphicgorillaz@gmail.com>
2025-07-10 02:56:04 +0200

    update .env

--- app/page.js
+++ app/page.js
@@ -5,23 +5,22 @@ import EditButton from './ui/shared/editButton';
 import SimpleBookTable from './ui/shared/booktable';
 
 async function getBooks() {
-    const results = [];
-    return results
-//    let client;
-//     try {
-//         client = await pool.connect();
-//         const result = await client.query(
-//             'SELECT books.*, authors.full_name AS author_name, genres.name AS genre_name FROM books INNER JOIN authors ON books.author_id = authors.author_id INNER JOIN genres ON books.genre_id = genres.genre_id ORDER BY book_id DESC');
-//         return result.rows;
-//     } catch (error) {
-//         // !!! THIS IS THE CRUCIAL PART !!!
-//         console.error('DATABASE ERROR DURING BUILD:', error); // Log the specific error
-//         // Important: Return an empty array or throw a custom error
-//         // if you want the page to error out. Returning [] allows it to build.
-//         return [];
-//     } finally {
-//         client.release();
-//     }
+
+   let client;
+    try {
+        client = await pool.connect();
+        const result = await client.query(
+            'SELECT books.*, authors.full_name AS author_name, genres.name AS genre_name FROM books INNER JOIN authors ON books.author_id = authors.author_id INNER JOIN genres ON books.genre_id = genres.genre_id ORDER BY book_id DESC');
+        return result.rows;
+    } catch (error) {
+        // !!! THIS IS THE CRUCIAL PART !!!
+        console.error('DATABASE ERROR DURING BUILD:', error); // Log the specific error
+        // Important: Return an empty array or throw a custom error
+        // if you want the page to error out. Returning [] allows it to build.
+        return [];
+    } finally {
+        client.release();
+    }
 }
 
 export default async function Home() {
