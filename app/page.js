8e8b806d9eabf11ffa4fab9e6baf8ab45fbe6e3e
masood17 <graphicgorillaz@gmail.com>
2025-07-12 18:19:41 +0200

    commenting out root add button

--- app/page.js
+++ app/page.js
@@ -30,13 +30,13 @@ export default async function Home() {
 
     return (
         <div>
-            <div className="flex justify-between items-center mb-6">
+            {/* <div className="flex justify-between items-center mb-6">
                 <h1 className="text-3xl font-bold">Library Catalog</h1>
                 <Link href="/dashboard/books/new" 
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Add New Book
                 </Link>
-            </div>
+            </div> */}
 
 
 <SimpleBookTable books={books}/>
