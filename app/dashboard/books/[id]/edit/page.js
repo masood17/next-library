ea18559f4f5b31685247416e379b495e36cb5a24
masood17 <graphicgorillaz@gmail.com>
2025-07-23 23:48:30 +0200

    create db query for book that is to be edited. page.js now queries for book to be edited.

--- app/dashboard/books/[id]/edit/page.js
+++ app/dashboard/books/[id]/edit/page.js
@@ -1,8 +1,13 @@
 // app/api/books/[id]/edit/page.js
+import {fetchBookByID} from '@/app/lib/data';
+
 export default async function Page(props){
     const params = await props.params;
     const id = params.id
     console.log(id);
+    const book = await fetchBookByID(id);
+    console.log(book);
+    
     return (
     
     
--- app/lib/data.js
+++ app/lib/data.js
@@ -17,7 +17,7 @@ export async function getBooks() {
             INNER JOIN
              publishers ON books.publisher_id = publishers.publisher_id
             ORDER BY
-             book_id DESC`);
+             book_id DESC;`);
         
         return result.rows;
     } catch (error) {
@@ -28,5 +28,37 @@ export async function getBooks() {
         return [];
     } finally {
         client.release();
+    }
+}
+
+
+export async function fetchBookByID(id){
+    let client;
+    client = await pool.connect();
+    try{
+        const result = await client.query(`
+            SELECT books.*, authors.full_name AS author_name, genres.name AS genre_name, publishers.name AS publisher_name
+            FROM
+             books 
+            INNER JOIN
+             authors ON books.author_id = authors.author_id 
+            INNER JOIN
+             genres ON books.genre_id = genres.genre_id 
+            INNER JOIN
+             publishers ON books.publisher_id = publishers.publisher_id
+            WHERE books.title_id = $1;
+            `, [id]);
+    if (result.rows.length > 0) {
+      return result.rows[0]; // Return the first (and only) book found
+    } else {
+      return null; // Return null if no book was found
+    }  
+    } catch(error){
+        console.log('DATABASE ERROR: ', error);
+        throw new Error('Failed to fetch book.');
+    } finally {
+        if(client){
+            client.release();
+        }
     }
 }
